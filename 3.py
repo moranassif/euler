854df0a1dcd92fff6a19e1ab8e585f2b78937041
Moran Assif <moranassif@gmail.com>
2015-03-27 12:40:55 +0300

    With problem 3

--- 3.py
+++ 3.py
@@ -1,1 +1,20 @@
 __author__ = 'Moran'
+import math
+from utils import is_prime
+
+def find_prime_factors(number):
+    """
+    Finds the prime factorials of a number by stupid iteration
+    :param number: A number of factor
+    :return: A list of factorials
+    """
+    factorials = []
+    for candidate in xrange(2, int(math.sqrt(number) + 1)):
+        if number % candidate == 0:
+            if is_prime(candidate):
+                factorials.append(candidate)
+            number /= candidate
+    return factorials
+
+
+print find_prime_factors(600851475143)
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
 __author__ = 'Moran'
 
 from fibonacci import fibonacci
+from primes import is_prime
