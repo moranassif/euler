84cdb64dd0d073cbb6c1eb8f4c307a2a5a788511
Moran Assif <moranassif@gmail.com>
2015-03-28 09:55:55 +0300

    With problem 7

--- 7.py
+++ 7.py
@@ -1,1 +1,17 @@
 __author__ = 'Moran'
+
+from utils import is_prime
+
+prime_number_place = 10001
+prime_number = None
+idx = 2
+number = 3
+
+while not prime_number:
+    if is_prime(number):
+        if idx == prime_number_place:
+            print number
+            break
+        else:
+            idx += 1
+    number += 2
