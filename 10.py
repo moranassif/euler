3650318d6fa8f951f89d0ac072106e16eac398fb
Moran Assif <moranassif@gmail.com>
2015-03-28 11:17:08 +0300

    With problem 10

--- 10.py
+++ 10.py
@@ -1,3 +1,13 @@
 __author__ = 'Moran'
+import math
 
 
+limit = 2000000
+
+sieve = range(limit+1)
+sieve[1] = 0
+for siever in xrange(2, int(math.sqrt(limit)) + 1):
+    for j in xrange(siever*2, limit+1, siever):
+        sieve[j] = 0
+
+print sum(sieve)
