9121746859221065a411037388e6a12ca3d8a05c
Moran Assif <moranassif@gmail.com>
2015-03-28 14:05:19 +0300

    With problem 12

--- 12.py
+++ 12.py
@@ -1,1 +1,21 @@
 __author__ = 'Moran'
+from utils import divisors
+
+num_of_divisors = 500
+even_idx = 2
+odd_idx = 1
+now_odd = True
+
+
+while True:
+    even_divisors = divisors(even_idx/2)
+    odd_divisors = divisors(odd_idx)
+    idx_divisors = {x*y for x in even_divisors for y in odd_divisors}
+    if len(idx_divisors) > num_of_divisors:
+        print odd_idx * even_idx / 2, idx_divisors
+        break
+    if now_odd:
+        odd_idx += 2
+    else:
+        even_idx += 2
+    now_odd = not now_odd
--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,19 @@ __author__ = 'Moran'
 from fibonacci import fibonacci
 from primes import is_prime, find_prime_factors
 from palindrom import is_palindrom
+import math
+
+
+def divisors(number):
+    """
+    Finds divisors of a given number
+    for 8 its 1,2,4,8
+    :param number: A natural number
+    :return: A list of divisors
+    """
+    divisors = set()
+    for i in xrange(1, int(math.sqrt(number))+1):
+        if number % i == 0:
+            divisors.add(i)
+            divisors.add(number/i)
+    return divisors
